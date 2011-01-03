2150fd02b8a997ce4b1ab652551fb856dee89e25
Noam Szpiro <hellonoam@gmail.com>
2011-01-04 01:43:08 +0200

    created toolsForTests

---
+++ chrome extension/front-end-tests/js/toolsForTests.js
@@ -0,0 +1,14 @@
+function getLocalhostWin() {
+	var winToOpen = new Object();
+	winToOpen.left = 10;
+	winToOpen.top = 10;
+	winToOpen.type = "normal";
+	winToOpen.height = 400;
+	winToOpen.incognito = false;
+	winToOpen.width = 400;
+	var tab = new Object();
+	tab.url = "http://localhost/";
+	tab.selected = false;
+	winToOpen.tabs = [tab, tab, tab];
+	return winToOpen;
+}
