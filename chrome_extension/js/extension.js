82525d6305816ed80e8b3bfc38516e4e36845805
Noam Szpiro <hellonoam@gmail.com>
2011-07-15 14:31:50 +0200

    polishing background.js added user class not being used yet

---
+++ chrome_extension/js/extension.js
@@ -0,0 +1,5 @@
+version
+restore
+serial
+getRandomSerial
+intervals
---
+++ chrome_extension/js/user.js
@@ -0,0 +1,84 @@
+/*
+ * this is a class that reprents a user
+ */
+ 
+ /*
+  * contructor for user
+  */
+var User = function(backup, username, nakedPass) {
+    if (!(this instanceof User)) //in case this was called as a function rather than a cons
+        return new User(...);
+
+    this.backup = backup;
+    this.username = username;
+    this.nakedPass = nakedPass;
+    this.password = tools.getLoginToken(this.nakedPass, this.username).toString();
+    this.windows;
+    this.lastSync = new Date();
+    this.loggedIn = false;
+    this.createBackup();
+}
+
+/*
+ * creates the user from the backup
+ */
+User.prototype.createUserFromBackup = function() {
+    this.password = this.backup.password;
+    this.username = this.backup.username;
+    this.nakedPass = this.backup.nakedPass;
+    this.MDK = JSON.parse(this.backup.MDK);
+    this.loggedIn = this.backup.loggedIn;
+    this.windows = this.backup.windows;//check this
+}
+
+/*
+ * creates a backup for the user
+ * by writing the fields of the user to 'backup'
+ * @item 
+ *  if only one item needs to be backed up then it is passed
+ *  if it's undefined all fields are backup up.
+ */
+User.prototype.createBackup = function(item) {
+    var stringArr = ["username", "password", "MDK", "windows", "nakedPass", "loggedIn"];
+    var argArr = [this.password, this.username, JSON.stringify(this.MDK),
+                  this.windows, this.nakedPass, this.loggedIn];
+    stringArr.forEach(function(value, index) {
+        if (!item || item == value)
+            this.backup.setItem(value, argArr[index]);
+    });
+}
+
+/*
+ * removes any traces of the user
+ */
+User.prototype.clear = function(backup) {
+    ["username", "password", "MDK", "windows", "nakedPass", "loggedIn"].forEach(
+        function(value, index) {
+            backup.removeItem(value);
+        }
+    );
+    this = undefined; //check that this works
+}
+
+/*
+ * indicates that the user has been successfully authenticated
+ */
+User.prototype.successfullyLoggedIn = function() {
+    this.loggedIn = true;
+    this.createBackup("loggedIn");
+}
+
+/*
+ * creates the MDK and saves it to the user
+ * @salt
+ *  the user specific salt
+ */
+User.prototype.setMDK = function(salt) {
+    console.log("in setMDK");
+    if (!this.password || this.password == "")
+        throw("invalid password");
+    this.MDK = tools.getMasterDataKey(this.nakedPass, salt);
+    if (this.MDK == [] || this.MDK == "")
+        throw("ERROR: bad MDK");
+    this.createBackup("MDK");
+}
